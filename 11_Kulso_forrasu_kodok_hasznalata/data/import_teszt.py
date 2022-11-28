1ff791c16ad36aad8da51259d43807f8710d4051
Dániel Mizsák <info@pythonvilag.hu>
2022-11-28 22:21:14 +0100

    Hide history for security reasons

---
+++ 11_Kulso_forrasu_kodok_hasznalata/data/import_teszt.py
@@ -0,0 +1,10 @@
+print('Sikeresen importálva: import_teszt.py')
+
+teszt_valtozo = 'titkos üzenet'
+
+def udvozles():
+	print('Szia! :)')
+	
+def plusz_ketto(szam):
+	eredmeny = szam + 2
+	return eredmeny
